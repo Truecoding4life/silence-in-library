9f0aa64d66b6f5d640402cc1e128ffe742993be7
nathangero <nathangero@yahoo.com>
2023-10-06 17:52:57 -0700

    implement search function

--- asset/js/script-results.js
+++ asset/js/script-results.js
@@ -7,40 +7,69 @@
 function getSearchQuery() {
     var queryResult = document.location.search;
     console.log("queryResult:", queryResult);
-
-    if (queryResult) {
-        getSearchResults(queryResult);
-    } else {
-        // If no result, go back to main page
-        document.location.replace("./index.html");
-    }
+    getSearchResults(queryResult);
+    // if (queryResult) {
+    //     getSearchResults(queryResult);
+    // } else {
+    //     // If no result, go back to main page
+    //     document.location.replace("./index.html");
+    // }
 }
 
 function getSearchResults(queryResult) {
 
-    // https://www.loc.gov/film-and-videos/?q=dog&fo=json
+    // https://www.loc.gov/?q=dog&fo=json
     // https://www.loc.gov//{format}/?fo=json
     // https://www.loc.gov/?q=basketball&fo=json&/audio
+    // ?q=asdf&format=audio
+    // ?q=asdf&
 
-    var format = "audio";
-    var query = "basketball";
-    var requestUrl = "https://www.loc.gov/search/?fo=json&q="+ query + "/format";
+    var split1 = queryResult.split("format=");
+    // console.log("split1:", split1);
+    var split2 = split1[0].split("=");
+    // console.log("split2:", split2);
+    var split3 = split2[1].split("&");
+    // console.log("split3:", split3)
+
+    var format = split1[1];
+    var query = split3[0];
+    console.log("format:", format, "query:", query);
+    search(query, format);
+}
+
+function search(query, format) {
+    var requestUrl = "https://www.loc.gov/search/" + format +"?fo=json&q="+ query;
     var thing;
     fetch(requestUrl)
         .then(function(response) {
-            console.log('return response is'+ response)
+            // console.log('return response is'+ response)
             return response.json();
         }).then(function(data){
-            console.log("data received is "+ data );
+            // console.log("data received is "+ data );
             console.log(requestUrl);
             thing = data;
             
-            for (var i = 0; i < 2; i++) {
+            $("#search-result").text(query.split("%20").join(" "));
+            $("#results-list").text("");
+            for (var i = 0; i < 10; i++) {
                 var title = data.results[i].title;
                 var date = data.results[i].date;
-                var description = data.results[i].description[0];
+                if (data.results[i].description[0]) {
+                    description = data.results[i].description[0];
+                }
 
-                console.log("title:", title, "date:", date, "description:", description);
+                var card = $('<div class="card"></div');
+                var cardHeader = $('<h2 class="card-header"></h2>');
+                cardHeader.text(title);
+                var cardDate = $('<p class="card-body"></p>');
+                cardDate.text(date);
+                var cardDescription = $('<p class="card-body"></p>');
+                cardDescription.text(description);
+
+                card.append(cardHeader, cardDate, cardDescription);
+                $("#results-list").append(card);
+
+                // console.log("title:", title, "date:", date, "description:", description);
             }
 
         })
@@ -50,70 +79,25 @@ $(function() {
     getSearchQuery()
 });
 
+$("#back").on("click", function(event) {
+    event.stopPropagation();
+    event.preventDefault();
+
+    document.location.replace("./index.html");
+})
+
 // Add eventlistner to the search botton
 $("#search").on("click",function(event) {
     event.preventDefault();
-    var searchedTerm = $("input").val();
-    var selectedFormat = $("#format").val();
-    var requestUrl = "https://www.loc.gov/search/" + selectedFormat +"?fo=json&q="+ searchedTerm;
-    var thing;
-    fetch(requestUrl)
-        .then(function(response) {
-            console.log('return response is'+ response)
-            return response.json();
-        }).then(function(data){
-            console.log("data received is "+ data );
-            console.log(requestUrl);
-            thing = data;
-            
-            for (var i = 0; i < 2; i++) {
-                var title = data.results[i].title;
-                var date = data.results[i].date;
-                var description = data.results[i].description[0];
+    var query = $("input").val();
+    var format = $("#format").val();
+    
+    search(query, format);
 
-                console.log("title:", title, "date:", date, "description:", description);
-            }
-
-        })
+    // Clear the variables
+    $("input").val("");
+    $("#format").val("");
 });
 
 
-
-// function getApi() {
-    
-//     var requestUrl = 'https://api.github.com/orgs/nodejs/repos';
-//     var thing;
-//     fetch(requestUrl)
-//       .then(function (response) {
-//         console.log("response", response)
-//         return response.json();
-//       })
-//       .then(function (data) {
-//         console.log("data",data)
-//         thing = data
-//         for (var i = 0; i < data.length; i++) {
-//           var createTableRow = document.createElement('tr');
-//           var tableData = document.createElement('td');
-//           var link = document.createElement('a');
-  
-//           // Setting the text of link and the href of the link
-//           console.log(data[i].html_url);
-//           link.textContent = data[i].html_url;
-//           link.href = data[i].html_url;
-  
-          
-//           tableData.appendChild(link);
-//           createTableRow.appendChild(tableData);
-//           tableBody.appendChild(createTableRow);
-     
-//         }
-//       }).catch(function(err){
-//         console.log(err);
-//       });
-//       console.log("Meow");
-//       console.log(thing);
-//   }
-  
-//   fetchButton.addEventListener('click', getApi);
-  
 
